8bbe391775604114ba29d5d9985a39c535042687
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-19 18:56:51 +0300

    Created basic user repository

---
+++ backend/src/user/user.repository.ts
@@ -0,0 +1,40 @@
+// src/modules/user/repositories/user.repository.ts
+
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findById(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  async createUser(userData: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(userData);
+    return this.userRepository.save(newUser);
+  }
+
+  async updateUser(id: number, userData: Partial<User>): Promise<User> {
+    await this.userRepository.update(id, userData);
+    return this.findById(id);
+  }
+
+  async deleteUser(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
