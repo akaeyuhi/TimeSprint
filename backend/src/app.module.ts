a043d27b3d624ba2dbfee706b74e3ea6ebacee49
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-20 02:09:26 +0300

    Added authmodule to root

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConnectionConfiguration } from 'ormconfig';
 import { UserModule } from './user/user.module';
 import { TeamModule } from './team/team.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(DatabaseConnectionConfiguration),
     UserModule,
     TeamModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
