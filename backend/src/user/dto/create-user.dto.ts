192172b033921c391fd2e82c4d49e99c9e24a433
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-19 22:41:11 +0300

    Described create-user.dto.ts

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsString,
+  Length,
+  Matches,
+  Min,
+} from 'class-validator';
+import { passwordRegex, UserRole } from 'src/user/utils';
+
+export class CreateUserDto {
+  @Length(8, 20)
+  @IsString()
+  username: string;
+  @IsEmail()
+  @IsString()
+  email: string;
+  @Matches(passwordRegex)
+  @IsString()
+  @Min(8)
+  password: string;
+  @Matches('password')
+  confirmPassword: string;
+  @IsEnum(UserRole)
+  role: UserRole = UserRole.COLLABORATOR;
+}
