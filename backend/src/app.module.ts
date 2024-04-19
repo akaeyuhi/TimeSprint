4b383238191c465e8393eb10cd48c583dd15bea1
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-19 18:02:43 +0300

    Added entities to config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Team } from 'src/entities/team.entity';
+import { LeisureActivity } from 'src/entities/leisure_activity.entity';
+import { Project } from 'src/entities/project.entity';
+import { Task } from 'src/entities/task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      entities: [User, Team, LeisureActivity, Project, Task],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
