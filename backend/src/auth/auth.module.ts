55d35fb21a3b11b1bc99b8b280ea8ec29067841b
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-20 02:07:09 +0300

    Basic auth module

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserService } from 'src/user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { AuthStrategy } from 'src/auth/auth.strategy';
+import { RefreshStrategy } from 'src/auth/auth-refresh.strategy';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({}),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UserService, AuthStrategy, RefreshStrategy],
+})
+export class AuthModule {}
