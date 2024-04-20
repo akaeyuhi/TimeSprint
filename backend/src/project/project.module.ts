a042d3ecab595cf1242f43d892e2d82768d05319
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-20 19:42:18 +0300

    Updated imports

--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
+import {UserModule} from "src/user/user.module";
+import {TeamModule} from "src/team/team.module";
+import {TeamService} from "src/team/team.service";
+import {TeamRepository} from "src/team/team.repository";
+import {ProjectRepository} from "src/project/project.repository";
 
 @Module({
+  imports: [TeamModule],
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [ProjectService, ProjectRepository, TeamRepository],
 })
 export class ProjectModule {}
