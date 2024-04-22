c40040d0ed88be05f296c7b1095d61c5f4698fcc
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-23 00:32:56 +0300

    Added task methods to userService

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -3,10 +3,16 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from 'src/user/user.repository';
 import { User } from 'src/user/entities/user.entity';
+import { CreateTaskDto } from 'src/task/dto/create-task.dto';
+import { TaskService } from 'src/task/task.service';
+import { Task } from 'src/task/entities/task.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly taskService: TaskService,
+  ) {}
 
   async findAll(): Promise<User[]> {
     return this.userRepository.findAll();
@@ -45,5 +51,20 @@ export class UserService {
 
   async findUserWithPassword(email: string) {
     return this.userRepository.getUserWithPassword(email);
+  }
+
+  async createUserTask(taskDTO: CreateTaskDto, userId: number): Promise<Task> {
+    const user = await this.userRepository.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    const newTask = await this.taskService.createTask(taskDTO);
+    return await this.userRepository.assignTaskToUser(user, newTask);
+  }
+
+  async deleteUserTask(userId: number, taskId: number): Promise<Task> {
+    const user = await this.userRepository.findById(userId);
+    const task = await this.taskService.findById(taskId);
+    return await this.userRepository.deleteTaskFromUser(user, task);
   }
 }
