ce7260ace2f63b11764b64d4e2f2b4d6f53939f3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-23 00:34:24 +0300

    Small refactor

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -3,14 +3,12 @@ import { TaskRepository } from './task.repository';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './entities/task.entity';
 import { ProjectService } from 'src/project/project.service';
-import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class TaskService {
   constructor(
     private readonly taskRepository: TaskRepository,
     private readonly projectService: ProjectService,
-    private readonly userService: UserService,
   ) {}
 
   async findTasksByProject(projectId: number): Promise<Task[]> {
@@ -18,11 +16,14 @@ export class TaskService {
   }
 
   async createTask(
-    projectId: number,
     createTaskDto: CreateTaskDto,
+    projectId?: number,
   ): Promise<Task> {
-    const project = await this.projectService.findProjectById(projectId);
-    return await this.taskRepository.createTask(project, createTaskDto);
+    if (projectId) {
+      const project = await this.projectService.findProjectById(projectId);
+      return await this.taskRepository.createTask(createTaskDto, project);
+    }
+    return await this.taskRepository.createTask(createTaskDto);
   }
 
   async findById(taskId: number): Promise<Task> {
