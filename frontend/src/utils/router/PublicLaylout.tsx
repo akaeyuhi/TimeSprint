be2b71e034329dc03c13249683267039f5b3f773
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-27 17:47:01 +0300

    Added layout to router

--- frontend/src/utils/router/PublicLaylout.tsx
+++ frontend/src/utils/router/PublicLaylout.tsx
@@ -1,13 +1,14 @@
 import { useStore } from 'src/hooks';
 import { observer } from 'mobx-react-lite';
 import { Navigate, useLocation } from 'react-router-dom';
+import Layout from "src/components/layout";
 
 const PrivateLayout = () => {
   const { isAuthenticated } = useStore('authStore');
   const location = useLocation();
 
   return isAuthenticated ? (
-      <></>
+      <Layout></Layout>
   ) : (
       <Navigate to="/auth/sign-in" state={{ from: location }} replace />
   );
