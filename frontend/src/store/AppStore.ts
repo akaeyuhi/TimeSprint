606976956d8ce1d21bd9257ef27fc378f1408821
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-27 16:27:13 +0300

    Initial linters config

---
+++ frontend/src/store/AppStore.ts
@@ -0,0 +1,19 @@
+import {makeAutoObservable} from 'mobx';
+
+class AppStore {
+  count = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count--;
+  }
+}
+
+export default new AppStore();
