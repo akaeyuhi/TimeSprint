93a7596a9d84559b908846163c0c4a59f7971d7d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-04-27 17:45:00 +0300

    Layout component

---
+++ frontend/src/components/layout/index.tsx
@@ -0,0 +1,27 @@
+import { Box, Stack } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../navbar';
+import Sidebar from '../sidebar';
+
+import { styles } from 'src/components/layout/styles';
+
+const Layout = () => {
+  return (
+      <Stack height="100vh" flexDirection="row" gap="50px">
+        <Stack sx={styles.siderWrapper} flex="0 0 250px">
+          <Sidebar />
+        </Stack>
+        <Stack flex="1">
+          <Box sx={styles.navbarWrapper}>
+            <Navbar />
+          </Box>
+          <Box sx={styles.mainWrapper}>
+            Main
+            <Outlet />
+          </Box>
+        </Stack>
+      </Stack>
+  );
+};
+
+export default Layout;
---
+++ frontend/src/components/layout/styles.ts
@@ -0,0 +1,14 @@
+import { createStyleSheet } from 'src/utils/theme/createStyleSheet';
+
+export const styles = createStyleSheet({
+  siderWrapper: {
+    background: '#fff',
+  },
+  navbarWrapper: {
+    background: '#c2c2c2',
+  },
+  mainWrapper: {
+    flex: 1,
+    background: '#c4c4c4',
+  },
+});
