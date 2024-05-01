51df8bb59bcb27316e338f876a0fe9be156b39d0
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-05-01 16:16:13 +0300

    Missed style files

---
+++ frontend/src/pages/Teams/components/CreateTeamForm/styles.ts
@@ -0,0 +1,11 @@
+import { createStyleSheet } from 'src/utils/theme/createStyleSheet';
+
+export const styles = createStyleSheet({
+  buttonContainer: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    mt: '1rem',
+  }
+});
