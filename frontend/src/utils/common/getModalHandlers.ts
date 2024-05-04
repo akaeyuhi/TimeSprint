529c3c4de8d18cb1018f904a60ca25ce1a2073dc
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-05-04 19:14:02 +0300

    helper function for handling multiple modals

---
+++ frontend/src/utils/common/getModalHandlers.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export interface ModalHandler {
+  isOpen: boolean,
+  open: () => void,
+  close: () => void;
+}
+
+export const getModalHandlers = <T extends object>(
+  modals: T,
+  set:  React.Dispatch<React.SetStateAction<T>>
+) => {
+  type ModalArray = [ keyof T,  boolean ];
+  const entries = Object.entries(modals) as ModalArray[];
+  const modalHandlers: Record<keyof T, ModalHandler> = {} as Record<keyof T, ModalHandler>;
+  entries.forEach(([name, fieldValue]) => {
+    modalHandlers[name] = {
+      isOpen: fieldValue,
+      open: () => set((state) => ({ ...state, [name]: true })),
+      close: () => set((state) => ({ ...state, [name]: false })),
+    };
+  });
+  return modalHandlers;
+};
