aa5efb93afdf934c7bddfd968fec0d3a16f0be75
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-05-07 14:13:40 +0300

    Added toasts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Router from 'src/utils/router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => (
   <div className="App">
     <Router/>
+    <ToastContainer autoClose={3000} position="bottom-right"/>
   </div>
 );
+
 
 export default App;
