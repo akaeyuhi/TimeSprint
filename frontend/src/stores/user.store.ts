1d500d25f3b7fa7062611cf877fb50b46fc11be2
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-05-10 18:39:39 +0300

    Mock tasks + name refactoring

--- frontend/src/stores/user.store.ts
+++ frontend/src/stores/user.store.ts
@@ -1,53 +1,87 @@
-import { action, get, makeObservable, observable } from 'mobx';
+import { action, get, makeAutoObservable, observable } from 'mobx';
 import { User } from 'src/models/user.model';
 import { CreateTaskDto } from 'src/dto/task/create-task.dto';
 import { Task } from 'src/models/task.model';
 import { Team } from 'src/models/team.model';
 import { UpdateTaskDto } from 'src/dto/task/update-task.dto';
+import { CreateTeamDto } from 'src/dto/team/create-team.dto';
+import TaskStore from 'src/stores/base.store';
 
 
-export class UserStore {
+export class UserStore extends TaskStore {
   @observable error: Error | null = null;
   @observable isLoading = false;
-  @observable authenticatedUser: User = { id: 8,
+  @observable currentUser: User = { id: 8,
     username: 'bob_jones',
     email: 'bob@example.com'
   } as User;
+  @observable tasks = [{
+    id: 1,
+    name: 'Complete project proposal',
+    description: 'Prepare and submit the project proposal by the deadline.',
+    urgency: true,
+    importance: true,
+    startDate: new Date('2024-04-19T13:00:00'),
+    endDate: new Date('2024-04-19T15:00:00'),
+    isCompleted: false,
+    dependencies: [] as Task[],
+  },
+  {
+    id: 2,
+    name: 'Review meeting notes',
+    description: 'Review the notes from yesterday\'s team meeting.',
+    urgency: false,
+    importance: true,
+    startDate: new Date('2024-04-19T15:30:00'),
+    endDate: new Date('2024-04-19T16:30:00'),
+    isCompleted: false,
+    dependencies: [] as Task[],
+  },];
 
 
   constructor() {
-    makeObservable(this);
+    super();
+    makeAutoObservable(this);
   }
 
   @get
   async getCurrentUser(): Promise<User> {
-    return this.authenticatedUser;
+    return this.currentUser;
   }
 
   @get
-  async fetchUser(userId = this.authenticatedUser.id): Promise<User> {
+  async fetch(userId = this.currentUser.id): Promise<void> {
     this.isLoading = true;
-    this.authenticatedUser = { id: 8,
-      username: 'bob_jones',
-      email: 'bob@example.com'
-    } as User;
+    this.currentUser = { ...this.currentUser};
+    this.tasks = this.currentUser.tasks;
     this.isLoading = false;
-    return this.authenticatedUser;
   };
 
-  @get
-  getUserTaskById(taskId: number): Task | null {
-    return this.authenticatedUser.tasks.find(task => task.id === taskId) ?? null;
+  async fetchByUsername(username: string): Promise<void> {
+    this.isLoading = true;
+    this.currentUser = { ...this.currentUser};
+    this.tasks = this.currentUser.tasks;
+    this.isLoading = false;
   }
 
   @get
-  getUserTasks(): Task[] {
-    return this.authenticatedUser.tasks;
+  getTaskById(taskId: number): Task | null {
+    return this.currentUser.tasks.find(task => task.id === taskId) ?? null;
+  }
+
+  @get
+  getTasks(): Task[] {
+    return this.currentUser.tasks;
+  }
+
+  @get
+  getUserTeamById(teamId: number): Team | null {
+    return this.currentUser.teams.find(team => team.id === teamId) ?? null;
   }
 
   @get
   getUserTeams(): Team[] {
-    return this.authenticatedUser.teams;
+    return this.currentUser.teams;
   }
 
   @action
@@ -55,14 +89,14 @@ export class UserStore {
     this.isLoading = true;
     const newTask = { ...task } as Task;
     this.isLoading = false;
-    this.authenticatedUser?.tasks.push(newTask);
+    this.currentUser?.tasks.push(newTask);
     return newTask;
   }
 
   @action
   async updateTask(taskId: number, taskDto: UpdateTaskDto) {
     this.isLoading = true;
-    const taskToUpdate = this.getUserTaskById(taskId);
+    const taskToUpdate = this.getTaskById(taskId);
     Object.assign(taskToUpdate!, taskDto);
     this.isLoading = false;
     return taskToUpdate;
@@ -71,16 +105,16 @@ export class UserStore {
   @action
   async deleteTask(taskId: number) {
     this.isLoading = true;
-    const newArray = this.authenticatedUser.tasks.filter(task => task.id !== taskId);
+    const newArray = this.currentUser.tasks.filter(task => task.id !== taskId);
     this.isLoading = false;
-    this.authenticatedUser.tasks = newArray;
+    this.currentUser.tasks = newArray;
     return taskId;
   }
 
   @action
   async assignTeam(team: Team) {
     this.isLoading = true;
-    this.authenticatedUser?.teams.push(team);
+    this.currentUser?.teams.push(team);
     this.isLoading = false;
     return team;
   }
@@ -88,21 +122,39 @@ export class UserStore {
   @action
   async leaveTeam(teamId: number) {
     this.isLoading = true;
-    this.authenticatedUser.teams = this.authenticatedUser.teams.filter(team => team.id !== teamId);
+    this.currentUser.teams = this.currentUser.teams.filter(team => team.id !== teamId);
     this.isLoading = false;
     return teamId;
   }
 
   @action
+  async createTeam(teamDto: CreateTeamDto) {
+    this.isLoading = true;
+    const newTeam = { ...teamDto } as Team;
+    this.currentUser.teams.push(newTeam);
+    this.isLoading = false;
+    return newTeam;
+  }
+  // @action
+  // async updateTeam(teamId: number, teamDto: UpdateTeamDto) {
+  //   this.isLoading = true;
+  //   const updated = this.getUserTeamById(teamId)!;
+  //   Object.assign(updated, teamDto);
+  //   this.isLoading = false;
+  //   return updated;
+  // }
+
+
+  @action
   setAuthenticatedUser(user: User) {
-    this.authenticatedUser = user;
+    this.currentUser = user;
   }
 
   @action
   async update(id: number, updateDto: Partial<User>): Promise<User> {
     this.isLoading = true;
-    const updatedUser = { ...this.authenticatedUser, ...updateDto };
-    this.authenticatedUser = updatedUser as User;
+    const updatedUser = { ...this.currentUser, ...updateDto };
+    this.currentUser = updatedUser as User;
     this.isLoading = false;
     return Promise.resolve(updatedUser as User);
   }
