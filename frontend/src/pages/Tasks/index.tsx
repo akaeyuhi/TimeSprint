ef14643f90faad1f436e260967ecfe4f30701313
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-05-10 18:51:32 +0300

    Deleted unused import

--- frontend/src/pages/Tasks/index.tsx
+++ frontend/src/pages/Tasks/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Box, Container, Stack, Typography } from '@mui/material';
 import { useStores } from 'src/hooks';
 import TaskSection from 'src/components/task/components/TaskSection';
