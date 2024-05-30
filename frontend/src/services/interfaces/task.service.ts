0eafac661f336fb6ba9de78b4254199baf4053b4
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-05-30 23:25:26 +0300

    TaskService interface to implement

---
+++ frontend/src/services/interfaces/task.service.ts
@@ -0,0 +1,13 @@
+import { CreateTaskDto } from 'src/services/dto/task/create-task.dto';
+import { Task } from 'src/models/task.model';
+import { UpdateTaskDto } from 'src/services/dto/task/update-task.dto';
+import { TaskContainer } from 'src/models/task-container.model';
+import { TaskReturn } from 'src/services/types';
+
+export interface ITaskService<T extends TaskContainer> {
+  getTasks(item: T): Promise<Task[] | null>;
+  createTask(dto: CreateTaskDto, item: T): Promise<TaskReturn>;
+  updateTask(dto: UpdateTaskDto, taskId: number): Promise<TaskReturn>;
+  deleteTask(taskId: number, item: T): Promise<TaskReturn>;
+  toggleTask(task: Task, item: T): Promise<TaskReturn>;
+}
