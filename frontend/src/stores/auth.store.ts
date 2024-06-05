fdb8d4571c2db2d7ef4b45deeeb224a248777409
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-05 03:52:24 +0300

    Implemented auth store methods

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -1,10 +1,12 @@
 import { makePersistable } from 'mobx-persist-store';
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import { Auth } from 'src/services/types';
 import { AuthService } from 'src/services/auth.service';
+import { LoginDto } from 'src/services/dto/auth/login.dto';
+import { RegisterDto } from 'src/services/dto/auth/register.dto';
 
 export class AuthStore {
-  auth: Auth = { user: { id: 3 } } as Auth;
+  auth: Auth = {} as Auth;
   error: Error | null = null;
   isLoading = false;
   authService: AuthService | null = null;
@@ -34,12 +36,69 @@ export class AuthStore {
     this.auth = {} as Auth;
   }
 
-  login() {
-    //TODO
-    return this.auth;
+  async login(authDto: LoginDto) {
+    if (!this.authService) {
+      throw new Error('AuthService not set');
+    }
+    this.isLoading = true;
+    try {
+      const auth = await this.authService.login(authDto);
+      runInAction(() => {
+        this.auth = auth as Auth;
+        this.error = null;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.error = error as Error;
+      });
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
+    }
   }
 
-  register() {
-    return this.auth;
+  async register(registerDto: RegisterDto) {
+    if (!this.authService) {
+      throw new Error('AuthService not set');
+    }
+    this.isLoading = true;
+    try {
+      const auth = await this.authService.register(registerDto);
+      runInAction(() => {
+        this.auth = auth as Auth;
+        this.error = null;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.error = error as Error;
+      });
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
+    }
+  }
+
+  async refreshToken(refreshToken: string) {
+    if (!this.authService) {
+      throw new Error('AuthService not set');
+    }
+    this.isLoading = true;
+    try {
+      const updatedAuth = await this.authService.refreshToken(refreshToken);
+      runInAction(() => {
+        Object.assign(this.auth, updatedAuth);
+        this.error = null;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.error = error as Error;
+      });
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
+    }
   }
 }
