d2124166d52b1e2ec3f909235e151a4fc1f7c172
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-05 03:56:03 +0300

    Actual store logic instead of mocks

--- frontend/src/pages/User/index.tsx
+++ frontend/src/pages/User/index.tsx
@@ -5,21 +5,25 @@ import TeamList from 'src/components/team/TeamList';
 import { useStores } from 'src/hooks';
 import { observer } from 'mobx-react';
 import Loader from 'src/components/loader';
+import { useParams } from 'react-router-dom';
 
 const UserPage: React.FC = () => {
   const { userStore: store, authStore } = useStores();
+  const { id } = useParams();
+
+  const userId = id ? parseInt(id) : authStore.auth.user.id;
 
   useEffect(() => {
-    store.fetch(1);
+    store.fetch(userId);
   }, []);
 
   if (store.isLoading || !store.current) return <Loader />;
+  if (store.error) throw store.error;
 
-  const isOwnPage = false && store.current?.id !== authStore.auth?.user?.id;
+  const isOwnPage = userId !== authStore.auth?.user?.id;
   const getWelcomeText = () => (isOwnPage ?
     (`Welcome, ${store.current && store.current?.username}`) :
     (`${store.current && store.current?.username}'s page`));
-
 
   return (
     <Container>
