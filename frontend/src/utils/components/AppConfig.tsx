65c3014b549764cea718d118e793a8a19e4d5825
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-05 03:52:47 +0300

    Error fallback component for handling critical errors

--- frontend/src/utils/components/AppConfig.tsx
+++ frontend/src/utils/components/AppConfig.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import 'react-toastify/dist/ReactToastify.css';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from 'src/utils/components/ErrorFallback';
 
 type Props = {
   children: ReactNode;
@@ -16,19 +18,22 @@ export const AppConfig: FC<Props> = ({ children }) => (
   <StrictMode>
     <BrowserRouter>
       <ThemeConfig>
-        <RootStoreProvider store={store}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            {children}
-          </LocalizationProvider>
-          <ToastContainer
-            position="bottom-right"
-            autoClose={3000}
-            hideProgressBar
-            closeOnClick
-            pauseOnFocusLoss
-            pauseOnHover
-            draggable />
-        </RootStoreProvider>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <RootStoreProvider store={store}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              {children}
+            </LocalizationProvider>
+            <ToastContainer
+              position="bottom-right"
+              autoClose={3000}
+              hideProgressBar
+              closeOnClick
+              pauseOnFocusLoss
+              pauseOnHover
+              draggable />
+          </RootStoreProvider>
+        </ErrorBoundary>
+
       </ThemeConfig>
     </BrowserRouter>
   </StrictMode>
---
+++ frontend/src/utils/components/ErrorFallback.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ErrorFallbackProps {
+  error: Error;
+  resetErrorBoundary: () => void;
+}
+
+const ErrorFallback: React.FC<ErrorFallbackProps> = ({ error, resetErrorBoundary }) => (
+  <div role="alert">
+    <p>Something went wrong:</p>
+    <pre>{error.message}</pre>
+    <button onClick={resetErrorBoundary}>Try again</button>
+  </div>
+);
+
+export default ErrorFallback;
