2e6cef1597d1ec56f23da9ce982d7768b7f70adc
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-13 00:30:20 +0300

    Turned form into generic and fixed dependency select

--- frontend/src/components/task/TaskForm/index.tsx
+++ frontend/src/components/task/TaskForm/index.tsx
@@ -15,53 +15,64 @@ import {
   FormHelperText,
 } from '@mui/material';
 import { styles } from 'src/components/modalForm/styles';
-import { DatePicker } from '@mui/x-date-pickers';
-import dayjs from 'dayjs';
-import { CreateTaskDto } from 'src/services/dto/task/create-task.dto';
+import { TaskDto } from 'src/services/dto/task/task.dto';
 import { Task } from 'src/models/task.model';
 import { User } from 'src/models/user.model';
 import { observer } from 'mobx-react';
 import { useValidation, ValidationErrors } from 'src/hooks/use-validation';
+import DependencySelector from 'src/components/task/DependencySelector';
+import CustomDatePicker from 'src/components/customDatePicker';
 
 interface CreateTaskFormProps {
-  onSubmit: (newTask: CreateTaskDto) => void;
-  tasks: Task[],
-  members?: User[],
+  onSubmit: (newTask: TaskDto, id?: number) => void;
   onCancel: () => void;
+  isEdited?: boolean
+  tasks: Task[],
+  task: Task | null,
+  members?: User[],
 }
 
-const validate = (state: CreateTaskDto): ValidationErrors<CreateTaskDto> => ({
+const validate = (state: TaskDto): ValidationErrors<TaskDto> => ({
   name: !(state.name && state.name.length > 8),
   description: !(state.description && state.description.length > 20),
   urgency: true,
   importance: true,
   startDate: !(state.endDate && state.startDate && state.startDate < state.endDate),
   endDate: !(state.endDate && state.startDate && state.startDate < state.endDate),
-  dependencies: !!state.dependencies
+  dependencies: !!state.dependencies,
 });
 
-const CreateTaskForm: React.FC<CreateTaskFormProps> = ({ tasks, members, onSubmit, onCancel }) => {
-  const [formData, setFormData, errors] = useValidation<CreateTaskDto>({
-    name: '',
-    description: '',
-    urgency: false,
-    importance: false,
-    startDate: new Date(),
-    endDate: new Date(Date.now() + 1),
-    dependencies: [],
+const selectStyle = {
+  maxHeight: 224,
+  width: 250,
+};
+
+const TaskForm: React.FC<CreateTaskFormProps> = ({
+  tasks,
+  task,
+  members,
+  onSubmit,
+  onCancel,
+  isEdited = false,
+}) => {
+  const [formData, setFormData, errors] = useValidation<TaskDto>({
+    name: task?.name ?? '',
+    description: task?.description ?? '',
+    urgency: task?.urgency ?? false,
+    importance: task?.importance ?? false,
+    startDate: task?.startDate ?? new Date(),
+    endDate: task?.endDate ?? new Date(),
+    dependencies: task?.dependencies ?? [],
+    user: task?.user,
   }, validate);
 
-  const selectStyle = {
-    maxHeight: 224,
-    width: 250,
-  };
-
   const isValid = !(errors.name && errors.description && errors.endDate && errors.startDate);
+  const isEditable = isEdited && !!task;
+  const tasksToAdd = task ? tasks.filter(item => item.id !== task.id) : tasks;
 
-  const handleDependencyChange = async (event: SelectChangeEvent) => {
-    const task = tasks.find(task => task.id === Number(event.target.value));
-    if (!task) return;
-    const dependencies = [...task.dependencies as [], task];
+  const handleDependencyChange = async (event: SelectChangeEvent<Task[]>) => {
+    const { target: value } = event;
+    const dependencies = [...value.value as Task[]];
     setFormData('dependencies', dependencies);
   };
 
@@ -76,13 +87,13 @@ const CreateTaskForm: React.FC<CreateTaskFormProps> = ({ tasks, members, onSubmi
     if (isValid)
       onSubmit({
         ...formData,
-      });
+      }, task?.id);
   };
 
   return (
     <Stack component="form" onSubmit={handleSubmit} sx={styles.container}>
       <Typography variant="h6" gutterBottom mb={2}>
-        Create Task
+        {isEditable ? 'Edit' : 'Create'} Task
       </Typography>
       <FormControl error={errors.name}>
         <InputLabel htmlFor="name">Task name</InputLabel>
@@ -129,58 +140,30 @@ const CreateTaskForm: React.FC<CreateTaskFormProps> = ({ tasks, members, onSubmi
         />} label="Importance" />
       </FormControl>
       <FormControl>
-        <DatePicker
-          label="Start date"
-          disablePast={true}
-          slotProps={{
-            textField: {
-              error: errors.startDate,
-              helperText: errors.startDate ? 'Start date should be before end' : '',
-            },
-          }}
-          onChange={(newValue) =>
-            setFormData('startDate', newValue?.toDate() ?? new Date())}
-          value={dayjs(formData.startDate)} />
+        <CustomDatePicker
+          formData={formData}
+          setFormData={setFormData}
+          errors={errors}
+          type="startDate"/>
       </FormControl>
       <FormControl>
-        <DatePicker
-          label="End date"
-          disablePast={true}
-          slotProps={{
-            textField: {
-              error: errors.endDate,
-              helperText: errors.endDate ? 'End date should be later than start' : '',
-            },
-          }}
-          onChange={(newValue) =>
-            setFormData('endDate', newValue?.toDate() ?? new Date())}
-          value={dayjs(formData.endDate)} />
+        <CustomDatePicker
+          formData={formData}
+          setFormData={setFormData}
+          errors={errors}
+          type="endDate"/>
       </FormControl>
       <FormControl>
         <InputLabel id="dependencies-label">Dependencies</InputLabel>
-        <Select
-          labelId="dependencies-label"
-          id="dependencies"
-          multiple
-          label="Dependencies"
-          value={formData.dependencies as unknown as string}
-          onChange={handleDependencyChange}
-          input={<OutlinedInput label="Dependencies"/>}
-          MenuProps={{
-            PaperProps: {
-              style: selectStyle,
-            },
-          }}
-        >
-          <MenuItem disabled value="">
-            Dependency
-          </MenuItem>
-          {tasks.map((task) => (
-            <MenuItem key={task.id} value={task.id}>
-              {task.name}
-            </MenuItem>
-          ))}
-        </Select>
+        <DependencySelector
+          tasks={tasksToAdd}
+          formData={formData}
+          handleChange={handleDependencyChange}
+          handleDependencyDelete={(task: Task) =>
+            setFormData('dependencies', formData.dependencies.filter((item) =>
+              item.id !== task.id))
+          }
+        />
       </FormControl>
       {members && members.length ? <FormControl>
         <InputLabel id="users-label">Assigned User</InputLabel>
@@ -209,7 +192,7 @@ const CreateTaskForm: React.FC<CreateTaskFormProps> = ({ tasks, members, onSubmi
       </FormControl> : <></>}
       <Box sx={styles.buttonContainer}>
         <Button variant="contained" color="primary" type="submit">
-          Create
+          {isEditable ? 'Edit' : 'Create'}
         </Button>
         <Button variant="outlined" color="secondary" onClick={onCancel}>
           Cancel
@@ -219,4 +202,4 @@ const CreateTaskForm: React.FC<CreateTaskFormProps> = ({ tasks, members, onSubmi
   );
 };
 
-export default observer(CreateTaskForm);
+export default observer(TaskForm);
