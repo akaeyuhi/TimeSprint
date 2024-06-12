c5fbb62dddfd91e9c36236e81a8550b9667509ab
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-12 20:11:36 +0300

    Added relation to method

--- backend/src/task/task.repository.ts
+++ backend/src/task/task.repository.ts
@@ -20,15 +20,16 @@ export class TaskRepository implements IRepository<Task> {
 
   async update(id: number, updateTaskDto: Partial<Task>): Promise<Task> {
     const task = await this.findById(id);
-    if (!task) {
-      throw new NotFoundException(`Task with ID ${id} not found`);
-    }
+    console.log(task, updateTaskDto);
     await this.repository.update(id, updateTaskDto);
     return { ...task, ...updateTaskDto };
   }
 
   async findById(id: number): Promise<Task> {
-    const task = await this.repository.findOneBy({ id });
+    const task = await this.repository.findOne({
+      where: { id },
+      relations: ['dependencies'],
+    });
     if (!task) {
       throw new NotFoundException(`Task with ID ${id} not found`);
     }
