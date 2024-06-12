332f6c7fdabca0ebaab9f7f3d772f2e42e852254
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-13 00:28:21 +0300

    Generic type to extend from

--- frontend/src/models/activity.model.ts
+++ frontend/src/models/activity.model.ts
@@ -3,8 +3,6 @@ import { Item } from 'src/models/item.model';
 import { DeadlineItem } from 'src/models/deadline-item.model';
 
 export interface LeisureActivity extends Item, DeadlineItem {
-  startDate: Date;
-  endDate: Date;
   isCompleted: boolean;
   user?: User;
 }
