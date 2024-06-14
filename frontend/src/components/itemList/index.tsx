cdc337e5df6c14f5d6b17f40d65a64c97d23bd9a
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-14 21:01:45 +0300

    Generic Item, List and Form to reduce code

---
+++ frontend/src/components/itemList/index.tsx
@@ -0,0 +1,34 @@
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+import { observer } from 'mobx-react';
+import { Item } from 'src/models/item.model';
+
+interface ItemListProps<T extends Item> {
+  items: T[];
+  ItemComponent: React.ComponentType<{ item: T } & any>;
+  itemComponentProps?: Omit<any, 'item'>; // Props for the ItemComponent excluding 'item'
+}
+
+const ItemList = <T extends Item>({
+  items,
+  ItemComponent,
+  itemComponentProps,
+}: ItemListProps<T>) => (
+  <>
+    {!items || items.length === 0 ? (
+      <Typography variant="h5" sx={{ mt: '1rem' }}>
+        No items available
+      </Typography>
+    ) : (
+      <Grid container spacing={2} sx={{ mt: '1rem' }}>
+        {items.map((item) => (
+          <Grid item xs={12} key={item.id}>
+            <ItemComponent item={item} {...itemComponentProps}/>
+          </Grid>
+        ))}
+      </Grid>
+    )}
+  </>
+);
+
+export default observer(ItemList);
