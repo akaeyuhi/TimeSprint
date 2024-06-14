02cf54347bcac76e1dbde0500181463ead8c4ce3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-14 20:59:21 +0300

    Added isOwnTask field

--- frontend/src/models/task.model.ts
+++ frontend/src/models/task.model.ts
@@ -1,13 +1,14 @@
 import { User } from 'src/models/user.model';
 import { Project } from 'src/models/project.model';
-import { Item } from 'src/models/item.model';
 import { DeadlineItem } from 'src/models/deadline-item.model';
+import { Item } from 'src/models/item.model';
 
 export interface Task extends Item, DeadlineItem {
   urgency: boolean;
   importance: boolean;
   isCompleted: boolean;
   dependencies: Task[];
+  isOwnTask: boolean;
   user?: User;
   project?: Project;
 }
