165c71fec09379963d8fce6d1e9817d071a5d62e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-06-14 21:02:12 +0300

    Safe loading of the user

--- frontend/src/pages/User/index.tsx
+++ frontend/src/pages/User/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Stack, Typography } from '@mui/material';
 import TaskSection from 'src/components/task/TaskSection';
 import TeamList from 'src/components/team/TeamList';
@@ -7,36 +7,46 @@ import { observer } from 'mobx-react';
 import Loader from 'src/components/loader';
 import { useParams } from 'react-router-dom';
 import { isObjectEmpty } from 'src/utils/common/isObjectEmpty';
+import { User } from 'src/models/user.model';
 
 const UserPage: React.FC = () => {
   const { userStore: store, authStore, handler } = useStores();
+  const [user, setUser] = useState({} as User);
   const { id } = useParams();
 
   const userId = id ? parseInt(id) : authStore.auth.user.id;
+  const isOwnPage = userId === authStore.auth?.user?.id;
 
   useEffect(() => {
-    store.fetch(userId);
-  }, [store, userId]);
+    if (isOwnPage && isObjectEmpty(store.current)) {
+      store.fetch(userId).then(() => {
+        setUser(store.current);
+      });
+    } else if (isOwnPage) {
+      setUser(store.current);
+    } else {
+      store.getUser(userId).then((user) => {
+        if (user) setUser(user);
+      });
+    }
+  }, [isOwnPage, store, userId]);
 
-  if (store.isLoading || isObjectEmpty(store.current)) return <Loader />;
+  if (store.isLoading || isObjectEmpty(user)) return <Loader />;
   if (store.error) handler.handle(store.error.message);
-  if (authStore.error) handler.handle(authStore.error.message);
 
-  const isOwnPage = userId !== authStore.auth?.user?.id;
-  const getWelcomeText = () =>
-    (isOwnPage ?
-      `Welcome, ${store.current && store.current?.username}` :
-      `${store.current && store.current?.username}'s page`);
+  const getWelcomeText = isOwnPage
+    ? `Welcome, ${user && user.username}`
+    : `${user && user.username}'s page`;
 
   return (
     <Container>
-      <Typography variant="h4">{getWelcomeText()}</Typography>
+      <Typography variant="h4">{getWelcomeText}</Typography>
       <TaskSection isEditable={isOwnPage} isProjectPage={false} />
       <Stack mt={4}>
         <Typography variant="h5">Teams</Typography>
       </Stack>
       <Stack>
-        <TeamList teams={store.current?.teams} />
+        <TeamList teams={user.teams} />
       </Stack>
     </Container>
   );
