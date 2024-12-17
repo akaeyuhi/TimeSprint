3d6ec84054c1e6eb9b3364124e122e61b277284b
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-12-17 13:50:24 +0200

    Endpoint for banning refresh token

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,10 +3,10 @@ import {
   Controller,
   Get,
   HttpStatus,
-  Post,
+  Post, Put,
   Req,
-  UseGuards,
-} from '@nestjs/common';
+  UseGuards
+} from "@nestjs/common";
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from '../user/dto/create-user.dto';
@@ -14,6 +14,9 @@ import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { JwtPayload } from 'src/auth/strategies/auth.strategy';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SiteAdminGuard } from "src/site-admin/guards/site-admin.guard";
+import { IsUserRole } from "src/site-admin/decorators/site-admin.decorator";
+import { AdminRole } from "src/user/utils";
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -45,7 +48,17 @@ export class AuthController {
   @ApiResponse({ status: HttpStatus.OK, description: 'Success' })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
   @UseGuards(AuthGuard('jwt-refresh'))
-  async refreshAccessToken(@Req() req: Request) {
-    return this.authService.refreshAccessToken(<JwtPayload>req.user);
+  async refreshAccessToken(@Req() req: Request, @Body() refreshToken: string) {
+    return this.authService.refreshAccessToken(<JwtPayload>req.user, refreshToken);
+  }
+
+  @Put('refresh')
+  @ApiOperation({ summary: 'Toggles refresh token status' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Success' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
+  @UseGuards(SiteAdminGuard)
+  @IsUserRole(AdminRole.ADMIN)
+  async toggleRefreshToken(@Body() refreshToken: string) {
+    return this.authService.banRefresh(refreshToken);
   }
 }
