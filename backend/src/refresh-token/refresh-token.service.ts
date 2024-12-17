45ebc0e86f3dd885e5a0e17608461854cf2f6c16
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2024-12-17 13:48:54 +0200

    Basic logic for refresh service

--- backend/src/refresh-token/refresh-token.service.ts
+++ backend/src/refresh-token/refresh-token.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateRefreshTokenDto } from './dto/create-refresh-token.dto';
 import { UpdateRefreshTokenDto } from './dto/update-refresh-token.dto';
+import { RefreshTokenRepository } from "src/refresh-token/refresh-token.repository";
 
 @Injectable()
 export class RefreshTokenService {
-  create(createRefreshTokenDto: CreateRefreshTokenDto) {
-    return 'This action adds a new refreshToken';
+  constructor(
+    private readonly refreshTokenRepository: RefreshTokenRepository,
+  ) {}
+  async create(createRefreshTokenDto: CreateRefreshTokenDto) {
+    return await this.refreshTokenRepository.create(createRefreshTokenDto);
   }
 
-  findAll() {
-    return `This action returns all refreshToken`;
+  async findAll() {
+    return await this.refreshTokenRepository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} refreshToken`;
+  async findOne(id: string) {
+    return await this.refreshTokenRepository.findById(id);
   }
 
-  update(id: number, updateRefreshTokenDto: UpdateRefreshTokenDto) {
-    return `This action updates a #${id} refreshToken`;
+  async findByToken(token: string) {
+    return await this.refreshTokenRepository.findByToken(token);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} refreshToken`;
+  async update(id: string, updateRefreshTokenDto: UpdateRefreshTokenDto) {
+    return await this.refreshTokenRepository.update(id, updateRefreshTokenDto);
+  }
+
+  async toggleBan(tokenValue: string) {
+    const token = await this.refreshTokenRepository.findByToken(tokenValue)
+    if (!token) {
+      throw new NotFoundException('Such token doesn\'t exist');
+    }
+    return await this.update(token.id, { isBanned: !token.isBanned})
+  }
+
+  async remove(id: string) {
+    return await this.refreshTokenRepository.delete(id);
+  }
+
+  async removeByValue(value: string) {
+    const token = await this.findByToken(value);
+    return await this.remove(token.id);
+  }
+
+  async isTokenExists(token: string) {
+    return !!await this.findByToken(token);
   }
 }
